a7298a1419887c65719f770181a85ffc58f91db3
sharpie <you@example.com>
2026-04-06 16:21:22 -0700

    rethink structs and stuff, better design

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,74 @@
-enum ChessPiece {
-    Pawn = 0,   // 000
-    Knight = 1, // 001
-    Bishop = 2, // 010
-    Rook = 3,   // 011
-    Queen = 4,  // 100
-    King = 5,   // 101
-
-    White = 8,
-    Black = 16,
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum PieceKind {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum Color {
+    White,
+    Black,
 }
 
-use std::{collections::HashMap, ops::BitOr};
+use std::{collections::HashMap, fmt, ops::BitOr};
 
-use ChessPiece as CP;
-
-impl BitOr for CP {
-    type Output = u8;
-
-    fn bitor(self, rhs: Self) -> Self::Output {
-        self as u8 | rhs as u8
-    }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ChessPiece {
+    kind: PieceKind,
+    color: Color,
 }
-struct Board {
-    board: HashMap<u8, u8>,
-}
+
+struct Board([Option<ChessPiece>; 64]);
+
 impl Board {
     fn from_fen(FEN: String) -> Self {
         let pieces = FEN.split_ascii_whitespace().take(1).collect::<String>();
-        let mut board = HashMap::new();
-        let mut board_index = 0;
+        let mut board: HashMap<u8, ChessPiece> = HashMap::new();
+        let mut board_index: u8 = 0;
 
         let mut piecesMap = HashMap::new();
-        piecesMap.insert("P", CP::Pawn | CP::White);
-        piecesMap.insert("N", CP::Knight | CP::White);
-        piecesMap.insert("B", CP::Bishop | CP::White);
-        piecesMap.insert("R", CP::Rook | CP::White);
-        piecesMap.insert("K", CP::King | CP::White);
-        piecesMap.insert("Q", CP::Queen | CP::White);
-
-        piecesMap.insert("p", CP::Pawn | CP::Black);
-        piecesMap.insert("n", CP::Knight | CP::White);
-        piecesMap.insert("b", CP::Bishop | CP::White);
-        piecesMap.insert("r", CP::Rook | CP::White);
-        piecesMap.insert("k", CP::King | CP::White);
-        piecesMap.insert("q", CP::Queen | CP::White);
+        piecesMap.insert('P', CPB::Pawn | CPB::White);
+        piecesMap.insert('N', CPB::Knight | CPB::White);
+        piecesMap.insert('B', CPB::Bishop | CPB::White);
+        piecesMap.insert('R', CPB::Rook | CPB::White);
+        piecesMap.insert('K', CPB::King | CPB::White);
+        piecesMap.insert('Q', CPB::Queen | CPB::White);
+        piecesMap.insert('p', CPB::Pawn | CPB::Black);
+        piecesMap.insert('n', CPB::Knight | CPB::Black);
+        piecesMap.insert('b', CPB::Bishop | CPB::Black);
+        piecesMap.insert('r', CPB::Rook | CPB::Black);
+        piecesMap.insert('k', CPB::King | CPB::Black);
+        piecesMap.insert('q', CPB::Queen | CPB::Black);
         for c in pieces.chars() {
             if let Some(n) = c.to_digit(10) {
-                board_index += n;
+                board_index += n as u8;
                 continue;
             }
 
+            board.insert(board_index, *piecesMap.get(&c).unwrap());
             board_index += 1;
         }
-        Self { board }
+        Self(board)
     }
 }
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for board_index in 0..64 {
+            self.0
+                .get(&board_index)
+                .unwrap_or(&(CPB::White | CPB::Black));
+        }
+        write!(f, "")
+    }
+}
+
 fn main() {
     let starting_board = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
 
     let board = Board::from_fen(starting_board);
+    println!("{}", board);
 }
