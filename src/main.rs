bf583deb1a207716abc69306f5810298327ac4e7
sharpie <you@example.com>
2026-04-06 17:23:28 -0700

    fancy board

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,11 @@ enum Color {
     Black,
 }
 
-use std::{collections::HashMap, fmt, ops::BitOr};
+use std::{
+    collections::HashMap,
+    fmt::{self, Display},
+    io,
+};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct ChessPiece {
@@ -21,54 +25,208 @@ struct ChessPiece {
     color: Color,
 }
 
+impl ChessPiece {
+    fn decode_fen(s: char) -> Option<Self> {
+        let color = if s.is_uppercase() {
+            Color::White
+        } else {
+            Color::Black
+        };
+
+        let kind = match s.to_ascii_lowercase() {
+            'p' => Some(PieceKind::Pawn),
+            'n' => Some(PieceKind::Knight),
+            'b' => Some(PieceKind::Bishop),
+            'r' => Some(PieceKind::Rook),
+            'k' => Some(PieceKind::King),
+            'q' => Some(PieceKind::Queen),
+            _ => None,
+        };
+
+        if kind.is_none() {
+            return None;
+        }
+
+        return Some(Self {
+            kind: kind.unwrap(),
+            color,
+        });
+    }
+    fn encode_fen(&self) -> char {
+        let mut c = match self.kind {
+            PieceKind::Pawn => 'p',
+            PieceKind::Bishop => 'b',
+            PieceKind::Knight => 'n',
+            PieceKind::Rook => 'r',
+            PieceKind::King => 'k',
+            PieceKind::Queen => 'q',
+        };
+
+        if self.color == Color::White {
+            c = c.to_ascii_uppercase();
+        }
+
+        c
+    }
+}
 struct Board([Option<ChessPiece>; 64]);
 
 impl Board {
-    fn from_fen(FEN: String) -> Self {
-        let pieces = FEN.split_ascii_whitespace().take(1).collect::<String>();
-        let mut board: HashMap<u8, ChessPiece> = HashMap::new();
+    fn from_fen(fen: String) -> Self {
+        let pieces = fen.split_ascii_whitespace().take(1).collect::<String>();
+        let mut board: [Option<ChessPiece>; 64] = [None; 64];
         let mut board_index: u8 = 0;
 
-        let mut piecesMap = HashMap::new();
-        piecesMap.insert('P', CPB::Pawn | CPB::White);
-        piecesMap.insert('N', CPB::Knight | CPB::White);
-        piecesMap.insert('B', CPB::Bishop | CPB::White);
-        piecesMap.insert('R', CPB::Rook | CPB::White);
-        piecesMap.insert('K', CPB::King | CPB::White);
-        piecesMap.insert('Q', CPB::Queen | CPB::White);
-        piecesMap.insert('p', CPB::Pawn | CPB::Black);
-        piecesMap.insert('n', CPB::Knight | CPB::Black);
-        piecesMap.insert('b', CPB::Bishop | CPB::Black);
-        piecesMap.insert('r', CPB::Rook | CPB::Black);
-        piecesMap.insert('k', CPB::King | CPB::Black);
-        piecesMap.insert('q', CPB::Queen | CPB::Black);
         for c in pieces.chars() {
+            if c == '/' {
+                continue;
+            }
+
             if let Some(n) = c.to_digit(10) {
                 board_index += n as u8;
                 continue;
             }
 
-            board.insert(board_index, *piecesMap.get(&c).unwrap());
+            board[board_index as usize] = ChessPiece::decode_fen(c);
             board_index += 1;
         }
+
         Self(board)
+    }
+
+    fn encode_tile(file: u8, rank: u8) -> u8 {
+        (7 - rank) * 8 + file
     }
 }
 
-impl fmt::Display for Board {
+impl Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for board_index in 0..64 {
-            self.0
-                .get(&board_index)
-                .unwrap_or(&(CPB::White | CPB::Black));
+        // Straigh up copying stockfish
+        let mut out = String::new();
+        for i in 0..64 {
+            if i % 8 == 0 {
+                if i > 0 {
+                    out.push('\n');
+                }
+                out.extend("+---+---+---+---+---+---+---+---+".chars());
+                out.push('\n');
+                out.push('|');
+            }
+
+            if let Some(p) = self.0[i] {
+                out.push(' ');
+                out.push(p.encode_fen());
+                out.extend(" |".chars());
+            } else {
+                out.extend("   |".chars());
+            }
+
+            if i % 8 == 7 {
+                out.push(' ');
+                out.extend((8 - (i / 8)).to_string().chars());
+            }
         }
-        write!(f, "")
+        out.extend("\n+---+---+---+---+---+---+---+---+\n".chars());
+        out.extend("  a   b   c   d   e   f   g   h".chars());
+
+        write!(f, "{}", out)
     }
+}
+
+struct Game {
+    board: Board,
+    white_turn: bool,
+}
+
+impl Game {
+    fn new() -> Self {
+        Self {
+            board: Board::from_fen(
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string(),
+            ),
+            white_turn: true,
+        }
+    }
+
+    fn try_move(&mut self, from: u8, to: u8) -> bool {
+        let from_piece = self.board.0[from as usize];
+        let to_piece = self.board.0[to as usize];
+
+        println!("{:?} {:?}", from_piece, to_piece);
+
+        if from_piece.is_none()
+            || from_piece.is_some()
+                && (from_piece.unwrap().color == Color::White) != self.white_turn
+            || (from_piece.is_some()
+                && to_piece.is_some()
+                && from_piece.unwrap().color == to_piece.unwrap().color)
+        {
+            return false;
+        }
+
+        // TODO: Actual piece move logic
+
+        self.board.0[from as usize] = None;
+        self.board.0[to as usize] = from_piece;
+        self.white_turn = !self.white_turn;
+
+        return true;
+    }
+}
+fn clear() {
+    print!("\x1B[2J\x1B[1;1H");
+}
+
+fn take_input() -> String {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap_or_default();
+
+    input.trim().to_lowercase().to_string()
 }
 
 fn main() {
-    let starting_board = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
+    let mut game = Game::new();
 
-    let board = Board::from_fen(starting_board);
-    println!("{}", board);
+    loop {
+        clear();
+        println!("{}", game.board);
+        println!("{}", game.white_turn);
+        let input = take_input();
+
+        if input.starts_with("mv") {
+            let parts = input.split_whitespace().collect::<Vec<&str>>();
+
+            if parts.len() != 3 {
+                continue;
+            }
+
+            let from = parts[1];
+            let to = parts[2];
+
+            if from.len() != to.len() || from.len() != 2 {
+                continue;
+            }
+
+            let files = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+            let from_file = files
+                .iter()
+                .position(|&c| c == from.chars().nth(0).unwrap())
+                .unwrap() as u8;
+            let from_rank = from.chars().nth(1).unwrap().to_digit(10).unwrap_or(0) as u8 - 1;
+
+            let to_file = files
+                .iter()
+                .position(|&c| c == to.chars().nth(0).unwrap())
+                .unwrap() as u8;
+            let to_rank = to.chars().nth(1).unwrap().to_digit(10).unwrap_or(0) as u8 - 1;
+
+            let from_tile = Board::encode_tile(from_file, from_rank);
+            let to_tile = Board::encode_tile(to_file, to_rank);
+
+            println!("{} {}", from_tile, to_tile);
+
+            game.try_move(from_tile, to_tile);
+        }
+    }
 }
